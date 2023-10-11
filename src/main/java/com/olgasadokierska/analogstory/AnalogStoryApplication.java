b6370fb233ff9a273ae9de0f0d0ad9693df6e832
OlgaSadokierska <115590776+OlgaSadokierska@users.noreply.github.com>
2023-10-11 16:20:28 +0200

    Add files via upload
    spring initializr

---
+++ src/main/java/com/olgasadokierska/analogstory/AnalogStoryApplication.java
@@ -0,0 +1,13 @@
+package com.olgasadokierska.analogstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalogStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalogStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olgasadokierska/analogstory/AnalogStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olgasadokierska.analogstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalogStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
