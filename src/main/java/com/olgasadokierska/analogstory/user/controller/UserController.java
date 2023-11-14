713d514ebf45845382a2d7758ce4762bea51c596
Olga <olga.sadokierska@op.pl>
2023-11-14 08:27:29 +0100

    controller update + data base init

--- src/main/java/com/olgasadokierska/analogstory/user/controller/UserController.java
+++ src/main/java/com/olgasadokierska/analogstory/user/controller/UserController.java
@@ -3,28 +3,26 @@ package com.olgasadokierska.analogstory.user.controller;
 import com.olgasadokierska.analogstory.user.model.User;
 import com.olgasadokierska.analogstory.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
+@RequestMapping("api/v1/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    @GetMapping("")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
-    @PostMapping("/user")
+    @PostMapping("/create")
     public User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
     }
 
-    @GetMapping("/users")
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+
 }
---
+++ target/classes/testData.sql
@@ -0,0 +1,4 @@
+insert into user (firstname,lastname, email, password, is_admin)
+values ('Olga','Sadokierska', 'olga.sadokierska@op.pl', MD5('olga'), true);
+insert into user (firstname,lastname, email, password, is_admin)
+values ('Izabela','Najder', 'izabela.najder@op.pl', MD5('izabela'), true);
