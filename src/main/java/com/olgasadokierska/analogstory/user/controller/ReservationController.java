ab233927b830083eb96018219f217e85fccc2e2d
IzabelaNajder <126763078+Izabela2001@users.noreply.github.com>
2023-12-18 17:56:17 +0100

    display reservation for logged with fun. contorl data

--- src/main/java/com/olgasadokierska/analogstory/user/controller/ReservationController.java
+++ src/main/java/com/olgasadokierska/analogstory/user/controller/ReservationController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @RestController
@@ -31,7 +33,26 @@ public class ReservationController {
 
             UserMediaDTO userMediaDTO = new UserMediaDTO();
             userMediaDTO.setReservations(reservations);
+
+            // Sprawdź, czy któraś rezerwacja ma expirationDate za 3 dni
+            for (Reservation reservation : reservations) {
+                LocalDateTime expirationDate = reservation.getExpirationDate();
+                LocalDateTime threeDaysBefore = LocalDateTime.now().plusDays(3);
+
+                if (expirationDate.truncatedTo(ChronoUnit.DAYS).isEqual(threeDaysBefore)) {
+
+                    System.out.println("Uwaga! Data ważności rezerwacji za 3 dni: " + expirationDate);
+                }
+
+                // Sprawdź, czy data ważności rezerwacji minęła
+                if (expirationDate.truncatedTo(ChronoUnit.DAYS).isBefore(LocalDateTime.now().truncatedTo(ChronoUnit.DAYS))) {
+
+                    System.out.println("Uwaga! Rezerwacja wygasła: " + expirationDate);
+                }
+            }
+
             return ResponseEntity.ok(userMediaDTO);
+
         }
     }
 
