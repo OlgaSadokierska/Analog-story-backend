86590f361c8e103ad3dbf9691f7a55366c54b190
olapp1 <olapp098@gmail.com>
2024-01-12 19:28:25 +0100

    improve of display media

--- target/generated-sources/annotations/com/olgasadokierska/analogstory/user/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/olgasadokierska/analogstory/user/mapper/UserMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-05T15:56:26+0100",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 20.0.2 (Oracle Corporation)"
+    date = "2024-01-11T17:55:08+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 18.0.2 (Amazon.com Inc.)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
